3b530df37844c56e27d7457b2fbddba1b5c1c395
eienblues <eienblues@gmail.com>
2016-07-29 17:12:21 +0400

    fast fix

--- process.php
+++ process.php
@@ -55,27 +55,12 @@ mysqli_query($link, $query2) or myerror($table_write, $link);
 
 //разъединяемся
 
-$query = "SELECT id, img, name, message, geo, date FROM $table WHERE name != '' AND message != '' ";
-$query2 = "SELECT id, img, name, message, geo, date FROM $tableX WHERE name != '' AND message != '' ";
-$query3 = "SELECT id, img, name, message, geo, date FROM $tableY WHERE name != '' AND message != '' ";
-
-
-$res = mysqli_query($link, $query) or myerror($table, $link);
-$res2 = mysqli_query($link, $query2) or myerror($tableX, $link);
-$res3 = mysqli_query($link, $query3) or myerror($tableY, $link);
-
-
-
-myoutput($res, "Main Table", "tstyle1");
-myoutput($res2, "Таблица 1", "tstyle2");
-myoutput($res3, "Таблица 2", "tstyle3");
+header("location:submitions.php");
 
 
 
 mysqli_close($link);
  
-
-echo ("<a href=\"index.php\">Вернуться</a>");
 
 	
 	}	else {  
